07d199ba0c8e26c629e59309f5a0f9fce43b6dcc
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-09 15:22:26 +0100

    routes added to practice express

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/", express.static("example"));
+
+app.use("/", express.static("home"));
+
+app.use("/about", express.static("about"));
+
+app.use("/contact", express.static("contact"));
+
 app.listen(5001, () => {
   console.log("Server is listening");
 });
