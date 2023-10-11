253328503aee8b8205ecd5284334e8126bbaf4ff
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-11 10:08:11 +0100

    enhancement: basic server complete

--- src/server.js
+++ src/server.js
@@ -34,33 +34,88 @@ const bookSchema = new mongoose.Schema({
 
 const Book = mongoose.model("book", bookSchema);
 
-app.get("/books", (request, response) => {
+app.get("/books", async (request, response) => {
   console.log(request.originalUrl);
-  const book = {
-    title: "the lord of the rings",
-    author: "tolkein",
-    genre: "fantasy",
-  };
+  const books = await Book.find({}, "title");
 
   const successResponse = {
-    message: "success",
-    book: book,
+    message: "Success!",
+    books: books,
   };
 
   response.send(successResponse);
 });
 
-app.post("/books", (request, response) => {
-  console.log(request.body.title);
-  const newBook = {
+app.post("/books", async (request, response) => {
+  const newBook = await Book.create({
     title: request.body.title,
     author: request.body.author,
     genre: request.body.genre,
-  };
+  });
 
   const successResponse = {
     message: "Success!",
     newBook: newBook,
+  };
+
+  response.send(successResponse);
+});
+
+// app.put("/books", async (request, response) => {
+//   const updateAuthor = await Book.findOneAndUpdate({
+//     title: request.body.title,
+//     author: request.body.author,
+//     genre: request.body.genre,
+//   });
+
+//   const successResponse = {
+//     message: "Success!",
+//     updateAuthor: updateAuthor,
+//   };
+
+//   response.send(successResponse);
+// });
+
+app.delete("/books", async (request, response) => {
+  const deleteBook = await Book.deleteOne({
+    title: request.body.title,
+  });
+
+  const successResponse = {
+    message: "Success!",
+    deleteBook: deleteBook,
+  };
+
+  response.send(successResponse);
+});
+
+// app.delete("/books", async (request, response) => {
+//   const criteria = {
+//     title: request.params.title,
+//     author: request.params.author,
+//     genre: request.params.genre,
+//   };
+
+//   const wipeDb = await Book.deleteMany(criteria);
+
+//   const successResponse = {
+//     message: "Success!",
+//     wipeDb: wipeDb,
+//   };
+
+//   response.send(successResponse);
+// });
+
+app.put("/books", async (request, response) => {
+  const updatedBook = await Book.findOneAndUpdate(
+    { title: request.body.title },
+    { [request.body.key]: request.body.value },
+    { new: true }
+  );
+
+  const successResponse = {
+    message: "Success!",
+    updatedBook: updatedBook,
   };
 
   response.send(successResponse);
