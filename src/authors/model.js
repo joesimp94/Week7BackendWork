d8e445b14ab266203ce6146279cd852a19a923e1
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-13 11:00:57 +0100

    enhancement: added authors stretch goals

---
+++ src/authors/model.js
@@ -0,0 +1,12 @@
+// const mongoose = require("mongoose");
+
+// const authorSchema = new mongoose.Schema({
+//   authorName: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// const Author = mongoose.model("author", authorSchema);
+
+// module.exports = Author;
---
+++ src/authors/routes.js
@@ -0,0 +1,10 @@
+// const { Router } = require("express");
+// const authorRouter = Router();
+
+// const Author = require("./model");
+
+// const { findAuthorByName } = require("./controllers");
+
+// authorRouter.get("./author", findAuthorByName);
+
+// module.exports = authorRouter;
